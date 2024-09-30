ceb496fc350c23bead57768e14021f6eff8baf59
Jord Gui [SSW] <41951199+Jord-Gui@users.noreply.github.com>
2024-09-30 11:01:10 +1000

    Initial commit

---
+++ test/serverless-api-leveraging-aws-services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessApiLeveragingAwsServices from '../lib/serverless-api-leveraging-aws-services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-api-leveraging-aws-services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessApiLeveragingAwsServices.ServerlessApiLeveragingAwsServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
